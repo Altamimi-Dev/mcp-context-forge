c79f50fd61e980d0ac717b6d973e83820eb911f2
Vishu Bhatnagar <vishu.bhatnagar@ibm.com>
2026-07-13 12:38:25 +0100

    chore: fix trailing whitespace from pre-commit hook
    Signed-off-by: Vishu Bhatnagar <vishu.bhatnagar@ibm.com>

--- tests/unit/mcpgateway/test_auth_logout.py
+++ tests/unit/mcpgateway/test_auth_logout.py
@@ -78,9 +78,9 @@ def setup_test_db(test_engine, test_session_factory):
     original_engine = mcpgateway.db.engine
     mcpgateway.db.SessionLocal = test_session_factory
     mcpgateway.db.engine = test_engine
-    
+
     yield
-    
+
     mcpgateway.db.SessionLocal = original_session_local
     mcpgateway.db.engine = original_engine
 
